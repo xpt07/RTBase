a0712d90b652494c03cf34dd2fa71207614fcb52
xpt07 <xpto7.pt@gmail.com>
2025-03-20 12:02:00 +0000

    Revert "Merge branch 'MSCGamesTom:main' into main"
    This reverts commit b3b8d25bfb91e4499d454c8554e702fc4c61e3ee, reversing
    changes made to 890e545a737b7d55cd86cea9e663f3f1ead45677.

--- RTBase/Scene.h
+++ RTBase/Scene.h
@@ -10,16 +10,13 @@
 class Camera
 {
 public:
-	Matrix projectionMatrix;
 	Matrix inverseProjectionMatrix;
 	Matrix camera;
-	Matrix cameraToView;
 	float width = 0;
 	float height = 0;
 	Vec3 origin;
 	void init(Matrix ProjectionMatrix, int screenwidth, int screenheight)
 	{
-		projectionMatrix = ProjectionMatrix;
 		inverseProjectionMatrix = ProjectionMatrix.invert();
 		width = (float)screenwidth;
 		height = (float)screenheight;
@@ -27,7 +24,6 @@ public:
 	void updateView(Matrix V)
 	{
 		camera = V;
-		cameraToView = V.invert();
 		origin = camera.mulPoint(Vec3(0, 0, 0));
 	}
 
@@ -42,21 +38,6 @@ public:
 		dir = camera.mulVec(dir);
 		dir = dir.normalize();
 		return Ray(origin, dir);
-	}
-	bool projectOntoCamera(const Vec3& p, float& x, float& y)
-	{
-		Vec3 pview = cameraToView.mulPoint(p);
-		Vec3 pproj = projectionMatrix.mulPointAndPerspectiveDivide(pview);
-		x = (pproj.x + 1.0f) * 0.5f;
-		y = (pproj.y + 1.0f) * 0.5f;
-		if (x < 0 || x > 1.0f || y < 0 || y > 1.0f)
-		{
-			return false;
-		}
-		x = x * width;
-		y = 1.0f - y;
-		y = y * height;
-		return true;
 	}
 };
 
