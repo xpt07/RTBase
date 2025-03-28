e418a620057369fd19fc7a159024b5c3fcce0c37
xpt07 <xpto7.pt@gmail.com>
2025-03-28 13:28:26 +0000

    changed scene

--- RTBase/Main.cpp
+++ RTBase/Main.cpp
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 	// runTests()
 	
 	// Initialize default parameters
-	std::string sceneName = "veach-bidir";
-	//std::string sceneName = "bathroom";
+	//std::string sceneName = "veach-bidir";
+	std::string sceneName = "bathroom";
 	//std::string sceneName = "MaterialsScene";
 	//std::string sceneName = "cornell-box";
 	std::string filename = "GI.hdr";
