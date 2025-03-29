0dc50f0ecdaccf8272f14454813a4d1f5f9774e3
xpt07 <101298390+xpt07@users.noreply.github.com>
2025-03-29 12:53:37 +0000

    fixed sampleDirectionFromLight

--- RTBase/Lights.h
+++ RTBase/Lights.h
@@ -183,9 +183,8 @@ public:
 	}
 	Vec3 sampleDirectionFromLight(Sampler* sampler, float& pdf)
 	{
-		// Add code to sample a direction from the light
-		Vec3 wi = Vec3(0, 0, 1);
-		pdf = 1.0f;
+		Vec3 wi = SamplingDistributions::cosineSampleHemisphere(sampler->next(), sampler->next());
+		pdf = SamplingDistributions::cosineHemispherePDF(wi);
 		Frame frame;
 		frame.fromVector(triangle->gNormal());
 		return frame.toWorld(wi);
